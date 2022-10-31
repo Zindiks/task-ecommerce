c26bf6ec3c946c286959b42c70eea951bca7f18b
Zindiks <103096207+Zindiks@users.noreply.github.com>
2022-10-31 12:12:28 +0200

    add localstorage

--- src/store/slices/cartSlice.js
+++ src/store/slices/cartSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, current } from '@reduxjs/toolkit';
 
 const initialState = {
-  selectedData: [],
+  selectedData: JSON.parse(localStorage.getItem('CART_DATA')),
 };
 
 const cartSlice = createSlice({
@@ -15,6 +15,7 @@ const cartSlice = createSlice({
       } else {
         state.selectedData.push({ ...payload, count: 1 });
       }
+      localStorage.setItem('CART_DATA', JSON.stringify(state.selectedData));
     },
     onClickMinus(state, { payload }) {
       const findItem = state.selectedData.find((item) => item.article === payload.article);
@@ -26,11 +27,14 @@ const cartSlice = createSlice({
       if (findItem.count < 1) {
         state.selectedData = state.selectedData.filter((item) => item !== findItem);
       }
+
+      localStorage.setItem('CART_DATA', JSON.stringify(state.selectedData));
     },
     cleanData(state) {
       console.log('TRANSFER DATA TO ANOTHER SERVER...!!!');
       console.log(current(state.selectedData));
-      state.selectedData = [];
+      localStorage.setItem('CART_DATA', JSON.stringify([]));
+      state.selectedData = JSON.parse(localStorage.getItem('CART_DATA'));
     },
   },
 });
